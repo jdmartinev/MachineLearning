d929136ac5c2b57f021ad01c896cf8e079e5d649
Juan David Martinez Vargas <jdmartinev@eafit.edu.co>
2023-09-01 17:18:23 -0500

    app

---
+++ Clase04/imageclassifier_fastapi/app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from fastai.learner import load_learner
+from fastai.vision.core import PILImage
+
+
+from huggingface_hub import hf_hub_download
+
+
+learner = load_learner(hf_hub_download("jdmartinev/intel_image_classification_fastai","model.pkl"))
+
+def classify_image(image):
+    img = PILImage.create(image)
+    pred_class, pred_idx, outputs = learner.predict(img)
+    return(f"Predicted class: {pred_class}")
+    
+demo = gr.Interface(classify_image, gr.Image(shape=(224, 224)), "text")
+demo.launch(share=True)
+
+
+
